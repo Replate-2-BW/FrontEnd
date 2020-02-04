1930cd5b686d326ddf33e7c02bf70c004accdbfa
Humayun Rashid <humayun@Humayuns-iMac.local>
2020-02-04 14:04:16 -0500

    Finished sign in form w/ submit. Thx Rick.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Formik, useFormik, Button, FormGroup, FormControl, ControlLabel } from 'formik';
+import { Formik, useFormik, Button, Form, FormGroup, FormControl, ControlLabel, Field } from 'formik';
 import axios from "axios";
 
 
@@ -13,24 +13,45 @@ onSubmit: values => {
     console.log("Values here: ", values);
     alert(JSON.stringify(values, null, 2));
     axios
-        .post("https://replate-2.herokuapp.com/api/register", values)
+        .post("https://replate-2.herokuapp.com/api/login", values)
         .then(res => console.log("This is axios.post.then res: ", res))
         .catch(err => console.log("This is axios.post.catch err: ", err))
 
  }
 });
-
+console.log(formik.values)
 return (
     <div>
-        <h2> Welcome to replate</h2>
-        <formik onSubmit={values => console.log(values)}>
+        <h2> Welcome to Replate</h2>
+        <Formik onSubmit={values => console.log(values)}>
             <Form onSubmit={formik.handleSubmit}>
                 <div>
+                    <label>username</label>
+                    <Field id="username"
+                           type="text"
+                           name="username"
+                           value={formik.values.username}
+                           onChange={formik.handleChange}
+                    />
+                </div>
+                <div>
+                    <label>password</label>
+                    <Field id="password"
+                           type="password"
+                           name="password"
+                           value={formik.values.password}
+                           onChange={formik.handleChange}
+                    />
+                </div>
+                <div>
+                    <button
+                    type="submit"
+                    >Submit</button>
 
                 </div>
             </Form>
+        </Formik>
 
-
-        </formik>
     </div>
 );
+}
