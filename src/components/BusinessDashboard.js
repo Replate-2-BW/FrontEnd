29d57bddbceb886c861f82c59ac5e9f5026346fe
thericktastic <love@The-RT-MBP.attlocal.net>
2020-02-04 13:19:01 -0500

    Successfully tested create a pickup, and created BusinessNavBar component

--- src/components/BusinessDashboard.js
+++ src/components/BusinessDashboard.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 // components
+import CreatePickupForm from "./CreatePickupForm";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 // This component displays the dashboard for the business users.
@@ -8,31 +9,61 @@ import { axiosWithAuth } from "../utils/axiosWithAuth";
 // The business dashboard shows the user the pickup requests created and has a nav bar at the bottom, allowing the user to navigate to:
 // Profile, create-a-pickup-request, and the user's created pickup requests
 
-const BusinessDashboard = props => {
-  const [pickups, setPickups] = useState([]);
+const initialState = [
+  {
+    typeOfFood: "Lettuce",
+    qty: "10",
+    preferredPickupTime: "3:00 PM"
+  },
+  {
+    typeOfFood: "Sweet Potatos",
+    qty: "25",
+    preferredPickupTime: "4:00 PM"
+  },
+  {
+    typeOfFood: "Lemons",
+    qty: "5",
+    preferredPickupTime: "4:30 PM"
+  }
+];
 
-  useEffect(() => {
-    axiosWithAuth()
-      .get("/auth/pickup/:id/biz")
-      .then(res => {
-        console.log(
-          "This is axiosWithAuth.get.then res in BusinessDashboard: ",
-          res
-        );
-      })
-      .catch(err => {
-        console.log(
-          "This is axiosWithAuth.get.catch err in BusinessDashboard: ",
-          err
-        );
-      });
-  }, []);
+const BusinessDashboard = props => {
+  const [pickups, setPickups] = useState(initialState);
+  console.log("This is pickups: ", pickups);
+
+  // useEffect(() => {
+  //   axiosWithAuth()
+  //     .get("/auth/pickup/:id/biz")
+  //     .then(res => {
+  //       console.log(
+  //         "This is axiosWithAuth.get.then res in BusinessDashboard: ",
+  //         res
+  //       );
+  //     })
+  //     .catch(err => {
+  //       console.log(
+  //         "This is axiosWithAuth.get.catch err in BusinessDashboard: ",
+  //         err
+  //       );
+  //     });
+  // }, []);
 
   return (
     <div>
       <h1>Replate</h1>
       <h3>Pick-Ups</h3>
-      <div></div>
+      <div>
+        {pickups.map(pickup => (
+          <div className="pickup">
+            <p>
+              {pickup.typeOfFood}, Amount: {pickup.qty}
+            </p>
+            <p>Preferred Pickup Time: {pickup.preferredPickupTime}</p>
+            <button>Edit</button>
+          </div>
+        ))}
+      </div>
+      <CreatePickupForm />
     </div>
   );
 };
