559719f6d5db5908d3c73a1d2c658a739790a1fe
Humayun Rashid <humayun@Humayuns-iMac.local>
2020-02-05 12:24:02 -0500

    created routes for all components

---
+++ src/components/BusinessRegistration.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Formik, Form, Field, useFormik } from "formik";
+import axios from "axios";
+
+// components
+
+// The purpose of this component is to allow the business user to create a new pickup request.
+// When the Create-A-Pickup button is tapped, the user is presented with the following fields:
+// Type of food, amount of food by count/weight, and a preferred pick up time
+
+export default function BusinessRegistration() {
+}
